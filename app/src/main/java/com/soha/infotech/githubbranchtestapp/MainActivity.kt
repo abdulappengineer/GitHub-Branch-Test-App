f10480866d9bac81a2da673cac3ade4ba3a1abab
abdul <abduluptu@gmail.com>
2025-05-01 12:00:21 +0530

    first commit

---
+++ app/src/main/java/com/soha/infotech/githubbranchtestapp/MainActivity.kt
@@ -0,0 +1,50 @@
+package com.soha.infotech.githubbranchtestapp
+
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.soha.infotech.githubbranchtestapp.ui.theme.GitHubBranchTestAppTheme
+import kotlin.math.log
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            GitHubBranchTestAppTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+            Log.d("TAG", "onCreate: First Commit")
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GitHubBranchTestAppTheme {
+        Greeting("Android")
+    }
+}
