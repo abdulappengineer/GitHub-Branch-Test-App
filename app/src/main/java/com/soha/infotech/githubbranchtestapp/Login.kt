796ed5073e58e0150b09fee7c77eaf9bb4d0a86e
abdul <abduluptu@gmail.com>
2025-05-06 10:17:16 +0530

    login screen added

---
+++ app/src/main/java/com/soha/infotech/githubbranchtestapp/Login.kt
@@ -0,0 +1,15 @@
+package com.soha.infotech.githubbranchtestapp
+
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun Login() {
+    Button(
+        onClick = { /* Handle click here */ }
+    ) {
+        Text(text = "Click Me")
+    }
+}
